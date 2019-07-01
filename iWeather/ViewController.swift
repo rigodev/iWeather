75583e38697263478cced29dcc03db436762c9c1
@rigodev <i.e.shuvalov@gmail.com>
2019-07-01 15:14:35 +0300

    Initial Commit

---
+++ iWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iWeather
+//
+//  Created by rigo on 01/07/2019.
+//  Copyright © 2019 Igor Shuvalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
